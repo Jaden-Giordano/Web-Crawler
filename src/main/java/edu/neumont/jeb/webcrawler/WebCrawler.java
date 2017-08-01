e09bb410142f51380a4d367c2b4eb7afadc4fe28
Emily.Parr <eparr@student.neumont.edu>
2017-07-31 21:26:22 -0600

    Remove Conjuctions
    Added method

--- src/main/java/edu/neumont/jeb/webcrawler/WebCrawler.java
+++ src/main/java/edu/neumont/jeb/webcrawler/WebCrawler.java
@@ -36,5 +36,23 @@ public class WebCrawler {
 		String[] links = r.getHTMLLinkURL(body, false, false);
 	}
 	
+	private void removeWords(String[] content) {
+		String[] conjuctions = {" and ", " it ", " but ", " or ", " though "
+				, " although ", " while ", " if ", " only ", " unless ", " until "
+				, " that ", " than ", " in ", " whether ", " as ", " whereas " 
+				, " by ", " the ", " till ", " when ", " whenever ", " because "
+				, " since ", " so ", " what ", " whichever ", " whose ", " also "
+				, " besides ", " furthermore ", " likewise ", " moreover ", " nevertheless "
+				, " nonetheless ", " conversely ", " instead ", " otherwise ", " rahter "
+				, " accordingly ", " consequently ", " hence ", " meanwhile "
+				, " therefore ", " thus "}; 
+		
+		for (String string : content) {
+			for (String conj : conjuctions) {
+				string.replace(conj, "");
+			}
+		}
+	}
+	
 	
 }
